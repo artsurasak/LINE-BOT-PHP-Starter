78f2849191910ce6140affe4788e9a2e6bae2c9a
Surasak <artvkm@gmail.com>
2018-03-09 15:50:48 +0700

    Commit

--- bot.php
+++ bot.php
@@ -1,21 +1,43 @@
 <?php
-require_once '../vendor/autoload.php';
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\FirePHPHandler;
-$logger = new Logger('LineBot');
-$logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-try {
-  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
-  error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
-  error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
-  error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
-  error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
-}
+	$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+	// Get POST body content
+	$content = file_get_contents('php://input');
+	// Parse JSON
+	$events = json_decode($content, true);
+	// Validate parsed JSON data
+	if (!is_null($events['events'])) 
+		{	
+		// Loop through each event	
+			foreach ($events['events'] as $event) 
+			{		// Reply only when message sent is in 'text' format		
+				if ($event['type'] == 'message' && $event['message']['type'] == 'text') 
+				{	// Get text sent			
+					$text = $event['message']['text'];			
+					// Get replyToken			
+					$replyToken = $event['replyToken'];			
+					// Build message to reply back			
+					$messages = [
+								 'type' => 'text',
+								 'text' => $text
+								];			
+					// Make a POST Request to Messaging API to reply to sender			
+					$url = 'https://api.line.me/v2/bot/message/reply';			
+					$data = [
+							 'replyToken' => $replyToken,
+							 'messages' => [$messages],			
+							];			
+					$post = json_encode($data);			
+					$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);			
+					$ch = curl_init($url);			
+					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");			
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);			
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $post);			
+					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);			
+					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);			
+					$result = curl_exec($ch);			
+					curl_close($ch);			
+					echo $result . "";		
+				}	
+			}
+		}
+echo "OK";
