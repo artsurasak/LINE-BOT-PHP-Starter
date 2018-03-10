e8ee051c9477351619765eba66e8eb8fece3ac97
Surasak <artvkm@gmail.com>
2018-03-10 22:35:38 +0700

    Commit

--- bot.php
+++ bot.php
@@ -21,27 +21,27 @@
 								 'text' => $text
 								];			
 					// Make a POST Request to Messaging API to reply to sender			
-					//$url = 'https://api.line.me/v2/bot/message/reply';			
-					$url = 'https://api.line.me/v2/bot/richmenu';
-					//$data = [
-					//		 'replyToken' => $replyToken,
-					//		 'messages' => [$messages],			
-					//		];			
-					$data = {
-							"richMenuId": "richmenu-88c05ef6921ae53f8b58a25f3a65faf7",
-							"size": {"width": 2500,"height": 1686},
-							"selected": false,
-							"name": "Nice richmenu",
-							"chatBarText": "Tap to open",
-							"areas": [
-									  {
-										"bounds": {"x": 0,"y": 0,"width": 2500,"height": 1686},
-										"action": {"type": "postback",
-												   "data": "action=buy&itemid=123"
-												  }
-									  }
-									 ]
-					}
+					$url = 'https://api.line.me/v2/bot/message/reply';			
+					//$url = 'https://api.line.me/v2/bot/richmenu';
+					$data = [
+							 'replyToken' => $replyToken,
+							 'messages' => [$messages],			
+							];			
+					//$data = {
+					//		"richMenuId": "richmenu-88c05ef6921ae53f8b58a25f3a65faf7",
+					//		"size": {"width": 2500,"height": 1686},
+					//		"selected": false,
+					//		"name": "Nice richmenu",
+					//		"chatBarText": "Tap to open",
+					//		"areas": [
+					//				  {
+					//					"bounds": {"x": 0,"y": 0,"width": 2500,"height": 1686},
+					//					"action": {"type": "postback",
+					//							   "data": "action=buy&itemid=123"
+					//							  }
+					//				  }
+					//				 ]
+					//}
 					$post = json_encode($data);			
 					$headers = array('Content-Type: application/json', 
 									 'Authorization: Bearer ' . $access_token
