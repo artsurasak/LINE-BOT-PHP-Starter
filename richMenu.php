523316a43fbd343510a07557af51f527947dfbd5
Surasak <artvkm@gmail.com>
2018-04-07 23:17:25 +0700

    callback

--- richMenu.php
+++ richMenu.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 	$access_token = 'wNGFPyRYMYL1ZuaxZBZN+gw/1FOMR52WrEGtybbWfLlXsyIJU+2NUrUB70DQzT1ID4Jhwh34P4mc3fFdIEI7rtjUToiUzOlxjmtEfS/mekbd01VYgAYZybnHi9y0Q3REK0oaVFJricEwTEehEYaOGgdB04t89/1O/w1cDnyilFU=';
 	// Get POST body content
 	$content = file_get_contents('php://input');
