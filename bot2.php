cb94f1560b61f58fea673ddd682525edd558b05d
Surasak <artvkm@gmail.com>
2018-04-18 11:55:49 +0700

    get user

--- bot2.php
+++ bot2.php
@@ -1,4 +1,18 @@
 <?php
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('wNGFPyRYMYL1ZuaxZBZN+gw/1FOMR52WrEGtybbWfLlXsyIJU+2NUrUB70DQzT1ID4Jhwh34P4mc3fFdIEI7rtjUToiUzOlxjmtEfS/mekbd01VYgAYZybnHi9y0Q3REK0oaVFJricEwTEehEYaOGgdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '553bc1a177fb9b042fa30b3544357169']);
+$response = $bot->getProfile('U0d589cbccf8f08124f85a5e2e86b8ce4');
+if ($response->isSucceeded()) {
+	echo 'Success'
+    $profile = $response->getJSONDecodedBody();
+    //echo $profile['displayName'];
+    //echo $profile['pictureUrl'];
+    //echo $profile['statusMessage'];
+}
+
+
+/*
 	$access_token = 'wNGFPyRYMYL1ZuaxZBZN+gw/1FOMR52WrEGtybbWfLlXsyIJU+2NUrUB70DQzT1ID4Jhwh34P4mc3fFdIEI7rtjUToiUzOlxjmtEfS/mekbd01VYgAYZybnHi9y0Q3REK0oaVFJricEwTEehEYaOGgdB04t89/1O/w1cDnyilFU=';
 	// Get POST body content
 	$content = file_get_contents('php://input');
@@ -39,67 +53,6 @@
 				curl_close($ch);			
 				echo $result . "";		
 			}
-	/*
-			$replyToken = $event['replyToken'];	
-			$url = 'https://api.line.me/v2/bot/message/reply';	
-			$headers = array('Content-Type: application/json', 
-									 'Authorization: Bearer ' . $access_token
-									 );
-			$messages = [
-						'type' => 'text',
-						'text' => 'Hello,user'
-						];		
-			$data = [
-						'replyToken' => $replyToken,
-						'messages' => [$messages],			
-							];	
-			$post = json_encode($data);		
-			$ch = curl_init($url);			
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");			
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);			
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);			
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);			
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);			
-			$result = curl_exec($ch);			
-			curl_close($ch);			
-			echo $result . "";		
-			
-			// Loop through each event	
-			foreach ($events['events'] as $event) 
-			{		// Reply only when message sent is in 'text' format		
-				if ($event['type'] == 'message' && $event['message']['type'] == 'text') 
-				{	// Get text sent			
-					$text = $event['message']['text'];			
-					// Get replyToken			
-					$replyToken = $event['replyToken'];			
-					// Build message to reply back			
-					$messages = [
-								 'type' => 'text',
-								 'text' => $text
-								 //'text' => 'Hello!'
-								];			
-					// Make a POST Request to Messaging API to reply to sender			
-					$url = 'https://api.line.me/v2/bot/message/reply';			
-					//$url = 'https://api.line.me/v2/bot/richmenu';
-					$data = [
-							 'replyToken' => $replyToken,
-							 'messages' => [$messages],			
-							];			
-				
-					$post = json_encode($data);			
-					$headers = array('Content-Type: application/json', 
-									 'Authorization: Bearer ' . $access_token
-									 );			
-					$ch = curl_init($url);			
-					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");			
-					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);			
-					curl_setopt($ch, CURLOPT_POSTFIELDS, $post);			
-					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);			
-					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);			
-					$result = curl_exec($ch);			
-					curl_close($ch);			
-					echo $result . "";		
-				}	
-			}*/
 		}
-echo "OK";
+		*/
+		
